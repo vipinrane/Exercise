c3685c4b56447e6b09661b34269fd816f9358891
vipinrane <vipinrane@gmail.com>
2014-09-15 16:02:25 +0530

    added.

---
+++ TestConsoleApplication/TestAbstract/TestAbstract/TImplementor/TImplementor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestAbstract.TInterface;
+
+namespace TestAbstract.TImplementor
+{
+    class TImplementor : UIAnimationHandler
+    {
+        public string TestMessage()
+        {
+            return "Implementor message";
+        }
+    }
+}
---
+++ TestConsoleApplication/TestAbstract/TestAbstract/TInterface/UIAnimationHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAbstract.TInterface
+{
+    public abstract class UIAnimationHandler
+    {
+        public string TestMessage()
+        {
+            return "Test";
+        }
+    }
+}
