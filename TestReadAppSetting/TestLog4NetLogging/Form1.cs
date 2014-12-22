afa06433b62935dd046405199441d6c661d94e2f
vipinrane <vipinrane@gmail.com>
2014-12-22 17:04:37 +0530

    modified.

---
+++ TestReadAppSetting/TestLog4NetLogging/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestLog4NetLogging
+{
+    public partial class Form1 : Form
+    {
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Program));
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
