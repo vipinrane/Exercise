bd4ee72ed5351be2d3064628a466e736c01b747d
vipinrane <vipinrane@gmail.com>
2014-12-23 13:04:13 +0530

    Modified.

--- TestReadAppSetting/TestLog4NetLogging/Form1.Designer.cs
+++ TestReadAppSetting/TestLog4NetLogging/Form1.Designer.cs
@@ -30,14 +30,35 @@
         private void InitializeComponent()
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
-            this.Menu = this.mainMenu1;
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.Text = "Form1";
             this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.button1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TestReadAppSetting/TestLog4NetLogging/Form1.cs
+++ TestReadAppSetting/TestLog4NetLogging/Form1.cs
@@ -11,11 +11,16 @@ namespace TestLog4NetLogging
 {
     public partial class Form1 : Form
     {
-        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Program));
+        private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(typeof(Form1));
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Log.Error("This is Compact Framework with log4net demo");
         }
     }
 }
